8956aeac111dd9e3d70c93362303a8e849791da4
Kevin Scott <94634901+keviddles@users.noreply.github.com>
2022-11-14 12:54:02 -0500

    Add example of bug

---
+++ index.ts
@@ -0,0 +1,100 @@
+import { LitElement, html, css } from 'lit';
+import { Router, Routes } from '@lit-labs/router';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('x-app')
+export class App extends LitElement {
+  static styles = [
+    css`
+    div {
+      background: rgba(0,0,0,0.1);
+      padding: 10px;
+    }
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    `
+  ];
+
+  private _router = new Router(this, [
+    {
+      path: '/*',
+      render: () => html`<x-parent></x-parent>`,
+    },
+  ]);
+
+  render() {
+    return html`
+      <div>
+        <p>App Container</p>
+        ${this._router.outlet()}
+      </div>
+    `;
+  }
+}
+
+@customElement('x-parent')
+class XParent extends LitElement {
+  static styles = [
+    css`
+    div {
+      background: rgba(0,0,0,0.1);
+      padding: 10px;
+    }
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    `
+  ];
+  private _routes = new Routes(this, [
+    {pattern: new URLPattern({ pathname: 'some-path/*?' }), render: () => html`<x-child></x-child>`},
+    {pattern: new URLPattern({ pathname: '' }), render: () => html`X-Parent Blank Match`},
+  ], {
+    fallback: {
+      render: () => html`X-Parent Fallback Route`,
+    }
+  });
+  render() {
+    return html`
+      <div>
+        <p>X-Parent</p>
+        ${this._routes.outlet()}
+      </div>
+    `;
+  }
+}
+
+@customElement('x-child')
+class XChild extends LitElement {
+  static styles = [
+    css`
+    div {
+      background: rgba(0,0,0,0.1);
+      padding: 10px;
+    }
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    `
+  ];
+  private _routes = new Routes(this, [
+    {path: 'child/foo', render: () => html`X-Child foo`},
+    {path: 'child/bar', render: () => html`X-Child bar`},
+    {path: '', render: () => html`X-Child Blank Match`},
+  ], {
+    fallback: {
+      render: () => html`X-Child Fallback Route`
+    }
+  });
+  render() {
+    return html`
+      <div>
+        <p>X-Child</p>
+        ${this._routes.outlet()}
+      </div>
+    `;
+  }
+}
